2c039bb673a27e82e88414530c168ef311998332
GiannisGl <ioannis.glampedakis@students.unibe.ch>
2017-05-13 22:53:23 +0200

    saved the caffemodel with torch format, to easily load it on cluster

--- loadCAM.lua
+++ loadCAM.lua
@@ -1,13 +1,14 @@
 require 'loadcaffe'
 require 'cudnn'
 require 'image'
-require 'itorch'
 
-model = loadcaffe.load('CAM/models/deploy_alexnetplusCAM_imagenet.prototxt', 'CAM/models/alexnetplusCAM_imagenet.caffemodel', 'cudnn')
+--model = loadcaffe.load('CAM/models/deploy_alexnetplusCAM_imagenet.prototxt', 'CAM/models/alexnetplusCAM_imagenet.caffemodel', 'cudnn')
+model = torch.load("alexnetplusCAM.torchmodel")
 print(model)
 
-input = image.load("CAM/dataset/new/001_im_prep.png", 3, "double")
+torch.save("alexnetplusCAM.torchmodel", model)
+
+--input = image.load("CAM/dataset/new/001_im_prep.png", 3, "double")
 
 --output = model:forward(input)
-
---image.display(input)
+--print(output)
