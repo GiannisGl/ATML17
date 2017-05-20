3b21c3a12ecc1a785e4e5af154c18d4d34357076
GiannisGl <ioannis.glampedakis@students.unibe.ch>
2017-05-20 16:19:24 +0200

    loadCAM file for cuda, that runs on the cluster

--- loadCAM.lua
+++ loadCAM.lua
@@ -1,26 +1,25 @@
-require 'loadcaffe'
-require 'cudnn'
-require 'image'
-require 'cutorch'
-require 'nn'
---matio = require 'matio'
+torch.setdefaulttensortype('torch.DoubleTensor')
 
 model = loadcaffe.load('CAM/models/deploy_alexnetplusCAM_imagenet.prototxt', 'CAM/models/alexnetplusCAM_imagenet.caffemodel', 'cudnn')
 --model = loadcaffe.load('CAM/models/deploy_googlenetCAM.prototxt', 'CAM/models/imagenet_googleletCAM_train_iter_120000.caffemodel', 'cudnn')
 --model = torch.load("alexnetplusCAM.torchmodel")
+model:insert(nn.Squeeze(),22)
+model:cuda()
 print(model)
---model:evaluate()
---torch.save("alexnetplusCAMnn.torchmodel", model)
 
-input = image.load("CAM/dataset/new/001_im_prep.png", 3, "double")
---input = matio.load("CAM/dataset/new/002_im_prep.mat", "prepinput")
+--torch.save("alexnetplusCAM.torchmodel", model)
 
---image.display(input)
-print(input:size())
+-- They don't have hdf5 installed
+--local myFile = hdf5.open('CAM/prepinput.h5', 'r')
+--local data = myFile:read('/data'):all()
+--myFile.close()
 
---input:cuda()
---print(input)
+data = image.load("CAM/dataset/new/001_im_prep.png", 3, "double")
 
---output = model:forward(input)
---print(output:size())
---print(output)
+-- convert to GPU
+cutorch.setDevice(1)
+data = data:cuda()
+--data = torch.load('CAM/prepinput.dat')
+
+output = model:forward(data)
+print(output)
